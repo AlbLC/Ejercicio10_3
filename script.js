028d0afc89cddc45cd8ac73667e955e794cb01f4
Alberto Lara <albertolaracaro@gmail.com>
2022-05-18 12:34:46 +0200

    +

--- script.js
+++ script.js
@@ -2,11 +2,25 @@
 //"name": null,
 //"public_repos": 5,
 
-function buscar(){
+function buscar() {
+    let parrafo=document.createElement("p");
+    // let texto=document.createTextNode();
     let nombre = document.getElementById("nombre").value;
+    let nombre1 = "";
+    let avatar1 = "";
+    let repos1 = "";
     fetch(`https://api.github.com/users/${nombre}`)
-    .then(res=>res.json())
-    .then(
-        
-    )
-}
+        .then(res => res.json())
+        .then(json =>  {let a = json.name
+            let b = json.avatar_url
+            let c = json.public_repos
+            var texto1 = document.createTextNode("Nombre: "+a+" Avatar: "+b+" Repo: "+c);
+            
+            parrafo.appendChild(texto1)
+            document.body.appendChild(parrafo)
+
+        }
+      
+            )
+       
+    }
